d06dfdfc73d470241601760cab2e3ba2afc32f5e
ABHINAVDEVELOPER620 <sinkumari91gmail.com>
2026-03-11 14:18:10 +0530

    club-game

--- ClubGame.py
+++ ClubGame.py
@@ -9,28 +9,32 @@ def age_checker():
         print("remember kid gambling is bad!")
         return False
 
-
 def game_of_clubs():
-        user_bet = int(input("enter amount to bet $ : "))
-        computer_guess =random.randint(1,11)
-        for attempts in range(1,11):
-            user_guess = int(input("enter a number to be guesse (1-11): "))
-            if user_bet < 0 :
-                print("enter a vaild amount")
-            else:
-                print(f"you have placed {user_bet}$ amount to bet" )
+    user_bet = int(input("enter amount to bet $ : "))
+    
+    if user_bet < 0:
+        print("enter a valid amount")
+    else:
+    
+        print(f"you have placed {user_bet}$ amount to bet")
+        computer_guess = random.randint(1, 11)
 
-                if computer_guess == user_guess :
-                    print(f"you have won the money you recived {user_bet}$")
-                    break
-                elif user_guess < computer_guess:
-                    print("higher!")
-                    print(f"you have {10- attempts} tries left.")
-                elif user_guess > computer_guess:
-                    print("lower")
+        
+        for attempts in range(1, 11):
+            user_guess = int(input("enter a number to be guessed (1-11): "))
+
+            if computer_guess == user_guess:
+                print(f"you have won! you received {user_bet}$")
+                break
+            elif user_guess < computer_guess:
+                print("higher!")
+                print(f"you have {10 - attempts} tries left.")
+            elif user_guess > computer_guess:
+                print("lower")
+                print(f"you have {10 - attempts} tries left.")
         else:
-            print(f"Game over! the number was{computer_guess}")
-
+          
+            print(f"Game over! the number was {computer_guess}")
 
 if age_checker():
      game_of_clubs()
